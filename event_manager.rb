e19396fab84d9fe1a8cb213dc1b0ab7e92de0b1a
Elise Worthy <elise.worthy@livingsocial.com>
2012-03-16 13:51:58 -0400

    added print_names method that accesses first and last name, added headers (attendee data now in hash format)

--- event_manager.rb
+++ event_manager.rb
@@ -3,12 +3,21 @@ require "csv"
 
 #Class Definitions
 class EventManager
+
   def initialize
     puts "EventManager Initialized."
     filename = "event_attendees.csv"
-    @file = CSV.open(filename)
+    @file = CSV.open(filename, {:headers => true})
   end
+
+  def print_names
+    @file.each do |line|
+      puts "#{line["first_Name"]} #{line["last_Name"]}"
+    end
+  end
+
 end
 
 #Script
 manager = EventManager.new
+manager.print_names
