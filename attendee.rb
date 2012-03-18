ffdc922f8fdab42b4e028b3d0d27799c4e68bd19
Elise Worthy <elise.worthy@livingsocial.com>
2012-03-18 14:56:31 -0400

    added attendee class and moved cleaning methods for zip and phone

---
+++ attendee.rb
@@ -0,0 +1,55 @@
+class Attendee
+  INVALID_PHONE = "0000000000"
+  INVALID_ZIPCODE = "00000"
+
+  attr_accessor :regdate, :first_name, :last_name, :email, :zipcode, :city, :state, :address, :homephone
+
+  def initialize(attributes)
+    self.regdate = attributes[:regdate]
+    self.first_name = attributes[:first_name]
+    self.last_name = attributes[:last_name]
+    self.email = attributes[:email_address]
+    self.zipcode = attributes[:zipcode]
+    self.city = attributes[:city]
+    self.state = attributes[:state]
+    self.address = attributes[:street]
+    self.homephone = attributes[:homephone]
+  end
+
+  def homephone=(original)
+    @homephone = clean_number(original)
+  end
+
+  def zipcode=(original)
+    @zipcode = clean_zipcode(original)
+  end
+
+  def clean_number(original)
+      number = original.delete "." " " "(" ")" "-"
+      
+      if number.length == 10
+        #do nothing
+      elsif number.length == 11
+        if number.start_with?("1")
+          number = number[1..-1]
+        else
+          number = INVALID_PHONE
+        end
+      else
+        number = INVALID_PHONE
+      end
+    return number
+  end
+
+  def clean_zipcode(original)
+    if original.nil?
+        result = INVALID_ZIPCODE
+    elsif original.length < 5
+      missing_zeros = 5 - original.length
+      result = "#{0 * missing_zeros}#{original}"
+    else
+      result = original
+    end
+    return result
+  end
+end
