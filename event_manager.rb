94ba13f368721fee9e8d6087e09a7438bb17b9e2
Elise Worthy <elise.worthy@livingsocial.com>
2012-03-23 14:45:30 -0400

    removed attendee methods from event_manager

--- event_manager.rb
+++ event_manager.rb
@@ -1,79 +1,36 @@
 #Dependencies
 require "csv"
 require "sunlight"
+require "./attendee"
 
 #Class Definitions
 class EventManager
-  INVALID_ZIPCODE = "00000"
-  INVALID_PHONE = "0000000000"
   Sunlight::Base.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
+  attr_accessor :attendees
 
   def initialize(filename)
     puts "EventManager Initialized."
-    @file = CSV.open(filename, {:headers => true, :header_converters => :symbol})
+    file = CSV.open(filename, {:headers => true, :header_converters => :symbol})
+    self.attendees = file.collect {|line| Attendee.new(line)}
   end
 
   def print_names
-    @file.each do |line|
-      puts "#{line[:first_name]} #{line[:last_name]}"
+    attendees.each do |attendee|
+      puts "First:#{attendee.first_name} Last:#{attendee.last_name}"
     end
   end
   
   def print_numbers
-    @file.each do |line|
-      number = clean_number(line[:homephone])
-      puts number
+    attendees.each do |attendee|
+      puts attendee.homephone
     end
   end
 
   def print_zipcodes
-    @file.each do |line|
-      zipcode = clean_zipcode(line[:zipcode])
-      puts zipcode
+    attendees.each do |attendee|
+      puts attendee.zipcode
     end
-  end
-
-  def clean_number(original)
-      number = original.delete "." " " "(" ")" "-"
-      
-      if number.length == 10
-        #do nothing
-      elsif number.length == 11
-        if number.start_with?("1")
-          number = number[1..-1]
-        else
-          number = INVALID_PHONE
-        end
-      else
-        number = INVALID_PHONE
-      end
-    return number
-  end
-
-  def clean_zipcode(original)
-    if original.nil?
-        result = INVALID_ZIPCODE
-    elsif original.length < 5
-      # Added 0's with a while loop
-      # while original.length < 5
-      #   original = "0" + original
-      # end
-      # result = original
-      
-      # Added 0's by calculating number of missing 0's
-      missing_zeros = 5 - original.length
-      result = "#{0 * missing_zeros}#{original}"
-
-      # Added a fixed number of zeros to front, then trimmed
-      # original = "000" + original
-      # result = original[-5..-1]
-
-      #Not sure how to buffer the string to a certain length with a method from the String API
-    else
-      result = original
-    end
-    return result
   end
 
   def output_data(filename)
@@ -159,6 +116,7 @@ class EventManager
   def state_stats
     state_data = {}
     @file.each do |line|
+      puts line.inspect
       state = line[:state]
       if state_data[state].nil?
         state_data[state] = 1
@@ -188,4 +146,4 @@ end
 
 #Script
 manager = EventManager.new("event_attendees.csv")
-manager.alpha_with_rank
+manager.print_zipcodes
